b5a5cfd1881cce4444b3cde524ef18e66010e848
Wang, Dawei <dwang@entertainment.com>
2018-03-02 09:59:55 -0500

    unittest incorporation

--- lambdas/test_logging/test_logging_test.py
+++ lambdas/test_logging/test_logging_test.py
@@ -1,11 +1,16 @@
+import unittest
 import boto3
 from moto import mock_ec2
 
 import test_logging
 
-@mock_ec2
-def test_test_logging():
-  test_logging.aws_work()
+
+class TestTestLogging(unittest.TestCase):
+
+  @mock_ec2
+  def test_test_logging(self):
+    test_logging.aws_work()
 
 if __name__ == "__main__":
-  test_test_logging()
+  #test_test_logging()
+  unittest.main()
